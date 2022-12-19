e80e5d40c3084030ee5d4d5d9fe13204873d0f48
Fidesnoella <fniragena@gmail.com>
2022-12-19 19:11:33 +0200

    fix: solving double quates error

--- components/Sections/AlumniStory.jsx
+++ components/Sections/AlumniStory.jsx
@@ -7,8 +7,8 @@ const AlumniStory = () => {
     return (
         <Wrapper>
             <div className='flex mt-28'>
-                <div className={`pl-5 gap-y-5 text-white bg-stone items-center w-[50%] h-[30%] mt-10 py-10`}>
-                    <h3 className='text-[1.75rem] font-bold leading-[2.1rem] pt-4 tracking-thin'>"Here I have met a lot of people who are breaking new ground"</h3>
+                <div className="pl-5 gap-y-5 text-white bg-stone items-center w-[50%] h-[30%] mt-10 py-10">
+                    <h3 className='text-[1.75rem] font-bold leading-[2.1rem] pt-4 tracking-thin'>&quot;Here I have met a lot of people who are breaking new ground&quot;</h3>
                     <p className='leading-7 pt-3'>Philip Häusser, alumnus</p>
                     <span className='mx-5'>
                         <Button style={"p-5"}>READ THE ALUMNI STORT</Button>
